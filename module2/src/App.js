0a871612d7bd2ca67c55cf8d458865afd5e32312
Neto Concon <concon.neto2@gmail.com>
2019-12-26 20:09:12 -0300

    add basic setup

--- module2/src/App.js
+++ module2/src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import './App.css';
+import Person from './Person/Person';
 
 class App extends Component {
   state ={
@@ -15,7 +16,7 @@ class App extends Component {
     this.setState({
       persons: [
         { name: 'Max', age: 28 },
-        { name: 'Manu', age: 29 },
+        { name: 'Neto', age: 29 },
         { name: 'Steh', age: 26 }
       ]
     })
@@ -24,7 +25,24 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <h1>App Working</h1>
+        <h1>Hi i am a react app</h1>
+        <p>This is really working</p>
+        <button onClick={() => this.switchNameHandler('Maximilian!!')}>
+          Switch Name
+        </button>
+        <Person
+        name={this.state.persons[0].name}
+        age={this.state.persons[0].age}
+        />
+        <Person
+        name={this.state.persons[1].name}
+        age={this.state.persons[1].age}
+        />
+        <Person
+        name={this.state.persons[2].name}
+        age={this.state.persons[2].age}
+        />
+
       </div>
     )
     }
