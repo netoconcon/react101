b34e03276b0776ef48301fcb947f1bbbb31b36d3
Neto Concon <concon.neto2@gmail.com>
2019-12-27 14:24:05 -0300

    list methdos

--- module2/src/Person/Person.js
+++ module2/src/Person/Person.js
@@ -1,12 +1,13 @@
 import React from 'react';
 
-const person = (props) => {
-  return (
-    <div>
-      <p> I am {props.name} and I am {props.age} </p>
-      <p> {props.children}</p>
-    </div>
-  )
+const person = ( props ) => {
+    return (
+        <div className="Person">
+            <p onClick={props.click}>I'm {props.name} and I am {props.age} years old!</p>
+            <p>{props.children}</p>
+            <input type="text" onChange={props.changed} value={props.name} />
+        </div>
+    )
 };
 
 export default person;
