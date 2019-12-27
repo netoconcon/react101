f71d165ce851cee0e78f9f32ac664e11b6b0e409
Neto Concon <concon.neto2@gmail.com>
2019-12-27 19:09:02 -0300

    styling

--- module3/src/Person/Person.js
+++ module3/src/Person/Person.js
@@ -1,14 +1,34 @@
 import React from 'react';
-import './Person.css';
+// import './Person.css';
+import styled from 'styled-components';
+
+const StyleDiv = styled.div`
+    width: 60%;
+    margin: 16px auto;
+    border-radius: 5px;
+    border: 1px solid #eee;
+    box-shadow: 0 2px 3px #ccc;
+    text-align: center;
+
+    @media (min-width: 500px) {
+            width: 450px;
+    }
+`
+;
 
 const person = ( props ) => {
-
+    const style ={
+        '@media (min-width: 500px)': {
+            width: '450px'
+        }
+    };
     return (
-        <div className="Person">
+        // <div className="Person" style={style}>
+        <StyleDiv>
             <p onClick={props.click}>I'm {props.name} and I am {props.age} years old!</p>
             <p>{props.children}</p>
             <input type="text" onChange={props.changed} value={props.name} />
-        </div>
+        </StyleDiv>
     )
 };
 
