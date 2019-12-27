165726698eeb3df8ff61b537c14966f618c20369
Neto Concon <concon.neto2@gmail.com>
2019-12-27 15:16:36 -0300

    wotking

--- module2-assignment/src/App.js
+++ module2-assignment/src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import './App.css';
 import Validation from './Validation/Validation';
+import Char from './Char/Char';
 
 class App extends Component {
   state = {
@@ -11,13 +12,27 @@ class App extends Component {
     this.setState({userInput: event.target.value});
   }
 
+  deleteCharHandler = (index) => {
+    const text = this.state.userInput.split('');
+    text.splice(index, 1);
+    const updatedText = text.join('');
+    this.setState({ userInput: updatedText});  }
+
   render() {
+    const charList = this.state.userInput.split('').map((ch, index) => {
+      return <Char 
+        character={ch} 
+        key={index}
+        clicked={() => this.deleteCharHandler(index)} />;
+    });
+
     return (
       <div className="App">
         <p>app working</p>
         <input type="text" onChange={this.inputChangeHandler} value={this.state.userInput} />
         <p>{this.state.userInput}</p>
         <Validation inputLength={this.state.userInput.length} />
+        {charList}
       </div>
     );
   }
