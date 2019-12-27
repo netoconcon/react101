f5ebbf3645927c6bc8c8a72428780d3e09f33a8e
Neto Concon <concon.neto2@gmail.com>
2019-12-26 22:22:08 -0300

    woring with lists

--- module2/src/Person/Person.js
+++ module2/src/Person/Person.js
@@ -3,7 +3,7 @@ import React from 'react';
 const person = (props) => {
   return (
     <div>
-      <p> I am a {props.name} and I am {props.age} </p>
+      <p> I am {props.name} and I am {props.age} </p>
       <p> {props.children}</p>
     </div>
   )
