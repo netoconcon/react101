0128d321ef5250d38a87c64a7a6cf84a1a3fe3dc
Neto Concon <concon.neto2@gmail.com>
2019-12-27 14:44:06 -0300

    validation working

--- module2-assignment/src/App.js
+++ module2-assignment/src/App.js
@@ -1,12 +1,23 @@
 import React, { Component } from 'react';
 import './App.css';
+import Validation from './Validation/Validation';
 
 class App extends Component {
+  state = {
+    userInput: ''
+  }
+
+  inputChangeHandler = (event) => {
+    this.setState({userInput: event.target.value});
+  }
 
   render() {
     return (
       <div className="App">
         <p>app working</p>
+        <input type="text" onChange={this.inputChangeHandler} value={this.state.userInput} />
+        <p>{this.state.userInput}</p>
+        <Validation inputLength={this.state.userInput.length} />
       </div>
     );
   }
