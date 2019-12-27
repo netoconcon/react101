a11b56698831a1a323fa3020a45e8f3ae84a2332
Neto Concon <concon.neto2@gmail.com>
2019-12-27 18:10:57 -0300

    dynamic css

--- module3/src/App.js
+++ module3/src/App.js
@@ -46,7 +46,8 @@ class App extends Component {
 
   render () {
     const style = {
-      backgroundColor: 'white',
+      backgroundColor: 'green',
+      color: 'white',
       font: 'inherit',
       border: '1px solid blue',
       padding: '8px',
@@ -68,12 +69,22 @@ class App extends Component {
           })}
         </div>
       );
+      style.backgroundColor = "red";
     }
+
+    const pClasses = [];
+    if (this.state.persons.length <=2){
+      pClasses.push('red');
+    }
+    if (this.state.persons.length <=1){
+      pClasses.push('bold');
+    }
+
 
     return (
       <div className="App">
         <h1>Hi, I'm a React App</h1>
-        <p>This is really working!</p>
+        <p className={pClasses.join(' ')}>This is really working!</p>
         <button
           style={style}
           onClick={this.togglePersonsHandler}>Toggle Persons</button>
